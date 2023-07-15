0bb5b1a1a4dedfd26783d302251cbf65e57275b1
Arman <sparmankhan@gmail.com>
2023-07-15 10:55:13 +0600

    error solve

--- pages/search/index.jsx
+++ pages/search/index.jsx
@@ -1,10 +1,9 @@
-import Search from '@/components/Search/Search';
 import Layout from '@/layout/Layout';
 
 const search = () => {
     return (
         <Layout title={'Search'}>
-            <Search />
+            {/* <Search /> */}
         </Layout>
     );
 };
