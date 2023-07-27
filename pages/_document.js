b460fb4a4d15b9abc3abf7b6c802580aaffd7a57
Arman Khan <sparmankhan@gmail.comm>
2023-07-27 14:06:18 +0600

    google meta added

--- pages/_document.js
+++ pages/_document.js
@@ -1,20 +1,19 @@
-
-import { Head, Html, Main, NextScript } from 'next/document';
-
-
+import { Head, Html, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head></Head>
-      <title>Arman's Blog</title>
-      <meta name="google-site-verification" content="ZiyBEiGqRODvrZAxabxhF6org3HqInfnHcOgi3KK4iQ" />
+      <Head>
+        <title>Arman's Blog</title>
+        <meta
+          name="google-site-verification"
+          content="ZiyBEiGqRODvrZAxabxhF6org3HqInfnHcOgi3KK4iQ"
+        />
+      </Head>
       <body>
-        
         <Main />
         <NextScript />
-       
       </body>
     </Html>
-  )
+  );
 }
