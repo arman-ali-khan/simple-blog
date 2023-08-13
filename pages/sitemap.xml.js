7e2d55dc63a87e1ef29b609684c5d6698cf68b0a
Arman Khan <sparmankhan@gmail.com>
2023-08-13 11:03:01 +0600

    sitemap update

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -6,7 +6,7 @@ function generateSiteMap(posts) {
   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:news="http://www.google.com/schemas/sitemap-news/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:mobile="http://www.google.com/schemas/sitemap-mobile/1.0" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xmlns:video="http://www.google.com/schemas/sitemap-video/1.1">
      <!--We manually set the two URLs we know already-->
      <url>
-       <loc>{${INTERNAL_DATA_URL}}</loc>
+       <loc>${INTERNAL_DATA_URL}</loc>
      </url>
      <url>
        <loc>${INTERNAL_DATA_URL}/search</loc>
