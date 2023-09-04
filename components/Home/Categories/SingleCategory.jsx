384c68f69c2bc9930770398d93d86f01be963fc8
Arman Khan <sparmankhan@gmail.com>
2023-09-04 11:59:41 +0600

    update category

--- components/Home/Categories/SingleCategory.jsx
+++ components/Home/Categories/SingleCategory.jsx
@@ -1,22 +1,22 @@
-import axios from 'axios';
 import Link from 'next/link';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 const SingleCategory = ({category}) => {
       // loading
       const [loading, setLoading] = useState(true);
       const [categoriesPost, setCategoriesPost] = useState({});
       const posts = categoriesPost.posts
-      useEffect(() => {
-          axios.get(`${process.env.NEXT_PUBLIC_API_PRO}/api/catetgory/${category.value}`)
-          .then(res=>{
-              setCategoriesPost(res.data)
-              setLoading(false)
-          })
-      },[loading,category.value])
+    //   useEffect(() => {
+    //       axios.get(`${process.env.NEXT_PUBLIC_API_PRO}/api/catetgory/${category.value}`)
+    //       .then(res=>{
+    //           setCategoriesPost(res.data)
+    //           setLoading(false)
+    //       })
+    //   },[loading,category.value])
             
-          return  (posts?.length> 0 && <li key={category.id} className={`border-b px-2 flex items-center `}>
-            <Link className="hover:text-blue-300 capitalize visited:text-purple-400 duration-300 text-blue-500 w-full flex px-3 py-2" href={`/category/${category.value}`}>{category.label} </Link> <span>({loading?0:posts.length || 0})</span>
+          return  ( <li key={category.id} className={`border-b px-2 flex items-center `}>
+            <Link className="hover:text-blue-300 capitalize visited:text-purple-400 duration-300 text-blue-500 w-full flex px-3 py-2" href={`/category/${category.value}`}>{category.label} </Link> 
+            {/* <span>({category?.post || 0})</span> */}
         </li>)
         
     
