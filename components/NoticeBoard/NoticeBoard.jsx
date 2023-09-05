a32239097a9dcd4d3ec04fc6a72a8cde133d1127
Arman Khan <sparmankhan@gmail.com>
2023-09-05 14:42:20 +0600

    initial commit

--- components/NoticeBoard/NoticeBoard.jsx
+++ components/NoticeBoard/NoticeBoard.jsx
@@ -22,7 +22,7 @@ const NoticeBoard = () => {
           <ul>
             {notice?.map((not) => {
               return (
-                <li className="leading-5">
+                <li className="leading-5 px-1">
                   {not?.notice}
                 </li>
               );
