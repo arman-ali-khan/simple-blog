0971a08d4de99b7a64605e92449084a4001facb2
Arman Khan <sparmankhan@gmail.com>
2023-09-06 06:50:13 +0600

    google analy added

--- firebase/firebase.config.js
+++ firebase/firebase.config.js
@@ -4,15 +4,19 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDhxjc0wacWprd3R2TYRp1_pIrmthHvc4M",
   authDomain: "simplw-blog.firebaseapp.com",
   projectId: "simplw-blog",
   storageBucket: "simplw-blog.appspot.com",
   messagingSenderId: "394022391579",
-  appId: "1:394022391579:web:4254a964c011daad710dbe"
+  appId: "1:394022391579:web:4254a964c011daad710dbe",
+  measurementId: "G-WLRN2J4SEM"
 };
 
 // Initialize Firebase
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
 export default app;
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import NextProgress from "next-progress";
-
+import Script from "next/script";
 import { Toaster } from "react-hot-toast";
 import ContextProvider from "../context/ContextProvider";
 import "../styles/globals.css";
@@ -7,6 +7,15 @@ import Effect from "./effect";
 export default function App({ Component, pageProps }) {
  
   return (
+    <>   
+<Script async src="https://www.googletagmanager.com/gtag/js?id=G-WLRN2J4SEM"></Script>
+<Script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments)}
+  gtag('js', new Date());
+
+  gtag('config', 'G-WLRN2J4SEM');
+</Script>
     <ContextProvider>
       <Component {...pageProps} />
       <Toaster position="top-center" reverseOrder={false} />
@@ -18,5 +27,6 @@ export default function App({ Component, pageProps }) {
       />
       <Effect />
     </ContextProvider>
+    </>
   );
 }
