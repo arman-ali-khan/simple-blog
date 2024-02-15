a7617210daf0f81efd5b4c46ee3835b13c3673d0
Arman Khan <sparmankhan@gmail.com>
2024-02-15 13:57:53 +0600

    error solved

--- lib/Loader.jsx
+++ lib/Loader.jsx
@@ -1,13 +1,13 @@
-import Lottie from "lottie-react";
+
 import React from 'react';
 import { RemoveScrollBar } from 'react-remove-scroll-bar';
-import Loading from '../public/images/loading.json';
 
 const Loader = () => {
     return (
         <div className='w-screen h-screen fixed top-0 flex justify-center items-center z-[9999]'>
             <div className="w-screen h-screen fixed top-0 left-0 flex justify-center items-center z-[9999999]">
-            <Lottie className="w-screen h-screen z-50 " animationData={Loading} loop={true} />
+            {/* <Lottie className="w-screen h-screen z-50 " animationData={Loading} loop={true} /> */}
+            Loading
             <RemoveScrollBar />
             </div>
         </div>
